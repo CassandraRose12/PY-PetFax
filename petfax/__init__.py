2ea4a7c5bccd368023cf3bcf181b7fae40dc75d0
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2023-02-03 18:03:33 -0500

    began styling

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -4,9 +4,13 @@ def create_app():
     app = Flask(__name__)
 
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
+   
     from . import pet
     app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     return app
